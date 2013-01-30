9210689cf86fe4c3d8c4f3c651af8765617c5562
towlerj <jtowler@gmail.com>
2013-01-30 19:21:42 +0000

    Initial Commit

---
+++ Vacation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Vacation
+//
+//  Created by jonathantowler on 30/01/2013.
+//  Copyright (c) 2013 jt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
